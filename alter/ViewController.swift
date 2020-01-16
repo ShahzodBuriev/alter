58dcb5c19ffd6085eef7884464502b066ae6fdb1
Shahzod <shakhzodb1@gmail.com>
2020-01-16 13:13:20 +0500

    Initial Commit

---
+++ alter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alter
+//
+//  Created by Shahzod Buriev on 1/16/20.
+//  Copyright © 2020 Shahzod Buriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
